ebe0d08c1fa583343879619302f3dbbeff8e50e2
darfux <darfux@163.com>
2014-08-09 10:29:37 +0800

    simple article implemented

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,9 @@ module ApplicationHelper
   def tf(*args)
     t(*args, scope: 'functional')
   end
+  def tp(content, scope)
+    t(content, scope: scope)
+  end
   def label_tag_t(name = nil, content_or_options = nil, options = nil, &block)
     label_tag(t(".#{name.to_s}"), content_or_options, options, &block)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :resources
   validates :account, presence: true, uniqueness: true
   validates_presence_of :password_confirmation, :on => :create
+
+  def articles
+    Article.joins("JOIN resources ON articles.id = resources.instance_id 
+        AND resources.instance_type = 'Article' ").where('resources.user_id = ?', id).order('created_at DESC')
+  end
 end
